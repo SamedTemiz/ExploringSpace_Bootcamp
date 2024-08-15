d02fff8b801e2dfc9175f87dae584e582c4d4c14
SamedTemiz <samedtemiz99@gmail.com>
2024-08-15 22:50:18 +0300

    Colors, dependencies and fonts have been adjusted.

---
+++ app/src/main/java/com/timrashard/exploringspace_bootcamp/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.timrashard.exploringspace_bootcamp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// App Theme Color
+val SpaceBlack = Color(0xFF000000)
+val SpacePurple = Color(0xFF8f53fc)
+val SpaceGray = Color(0xFF1a1a1a)
+val SpaceWhite = Color(0xFFFFFFFF)
